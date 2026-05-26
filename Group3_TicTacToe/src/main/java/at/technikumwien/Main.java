c5a23f5b7d83431fa7fccf67b43c13cd52f97327
Kevin Schuster <schusterkevin98@gmail.com>
2026-05-26 20:07:10 +0200

    Add game end notification & loop till the game ends

--- Group3_TicTacToe/src/main/java/at/technikumwien/Main.java
+++ Group3_TicTacToe/src/main/java/at/technikumwien/Main.java
@@ -1,40 +1,67 @@
 package at.technikumwien;
 
-
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // game initialize - newGame holds player and board data
         TicTacToe newGame = new TicTacToe();
-        newGame.player1 = new Player('X');
-        newGame.player2 = new Player('O');
-        newGame.board = new Board();
 
-        // print msg
         newGame.start();
 
-        // game loop - place holder - 9 possible moves in total - has to be updated to
-        int turnCounter = 0;
-
-        while (turnCounter <= 9){
+        // The game continues until there is a winner or a draw.
+        while (!newGame.isGameOver()) {
             System.out.println("Current Player: " + newGame.currentPlayer.getMarker());
             newGame.board.print();
 
             boolean legalMove;
+
+            // Repeats the input until the player chooses a valid and empty cell.
             do {
-                System.out.println("row (0-2): ");
-                int rowInput = sc.nextInt();    // no wrong Input check
-                System.out.println("col (0-2): ");
-                int colInput = sc.nextInt();    // no wrong Input check
+                int rowInput = readValidNumber(sc, "row (0-2): ");
+                int colInput = readValidNumber(sc, "col (0-2): ");
+
                 legalMove = newGame.board.place(rowInput, colInput, newGame.currentPlayer.getMarker());
-            } while(!legalMove);
 
+                if (!legalMove) {
+                    System.out.println("This cell is already occupied. Please choose another cell.");
+                }
 
-            newGame.switchCurrentPlayer();
-            turnCounter++;
+            } while (!legalMove);
+
+            // After every valid move, the game checks if it has ended.
+            if (newGame.isGameOver()) {
+                newGame.board.print();
+                newGame.printGameEndNotification();
+            } else {
+                newGame.switchCurrentPlayer();
+            }
+        }
+
+        sc.close();
+    }
+
+    // Reads a number from the user and checks if it is between 0 and 2.
+    private static int readValidNumber(Scanner sc, String message) {
+        int input;
+
+        while (true) {
+            System.out.println(message);
+
+            if (sc.hasNextInt()) {
+                input = sc.nextInt();
+
+                if (input >= 0 && input <= 2) {
+                    return input;
+                } else {
+                    System.out.println("Invalid input. Please enter a number between 0 and 2.");
+                }
+            } else {
+                System.out.println("Invalid input. Please enter a number.");
+                sc.next();
+            }
         }
     }
 }
--- Group3_TicTacToe/src/main/java/at/technikumwien/TicTacToe.java
+++ Group3_TicTacToe/src/main/java/at/technikumwien/TicTacToe.java
@@ -7,16 +7,20 @@ public class TicTacToe {
     Player currentPlayer;
     Board board;
 
-    public TicTacToe(){
-
-    }
-
-    public void start(){
-        System.out.print("*** Tic Tac Toe *** \n");
+    public TicTacToe() {
+        this.player1 = new Player('X');
+        this.player2 = new Player('O');
         this.currentPlayer = this.player1;
+        this.board = new Board();
     }
 
-    public void switchCurrentPlayer(){
+    // Starts the game and prints the title.
+    public void start() {
+        System.out.print("*** Tic Tac Toe *** \n");
+    }
+
+    // Switches the current player after a valid move.
+    public void switchCurrentPlayer() {
         if (currentPlayer == this.player1) {
             this.currentPlayer = this.player2;
         } else {
@@ -24,7 +28,27 @@ public class TicTacToe {
         }
     }
 
-    public boolean hasWinner(){
-        return true;
+    // Checks if the current player has won.
+    public boolean hasWinner() {
+        return this.board.hasWinner(this.currentPlayer.getMarker());
+    }
+
+    // Checks if the game ended in a draw.
+    public boolean isDraw() {
+        return this.board.isFull() && !hasWinner();
+    }
+
+    // Checks if the game is over because of a win or a draw.
+    public boolean isGameOver() {
+        return hasWinner() || isDraw();
+    }
+
+    // Prints the final game result.
+    public void printGameEndNotification() {
+        if (hasWinner()) {
+            System.out.println("Player " + this.currentPlayer.getMarker() + " wins!");
+        } else if (isDraw()) {
+            System.out.println("The game ended in a draw!");
+        }
     }
 }
